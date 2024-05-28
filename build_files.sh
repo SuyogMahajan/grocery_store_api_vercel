606b15f7f86b80e4795ac4671c691f258953b1b0
Suyog Mahajan <86179143+SuyogMahajan@users.noreply.github.com>
2024-05-28 10:40:22 +0000

    trying connecting postgrec database

---
+++ build_files.sh
@@ -0,0 +1,9 @@
+echo "Building Project"
+python3.10 -m pip install -r requirement.txt
+
+echo 'Making Migration"
+python3.10 manage.py makemigrations --noinput
+python3.10 manage.py migrate --noinput 
+
+echo "Collect Static"
+python3.10 manage.py collectstatic --noinput --clear
